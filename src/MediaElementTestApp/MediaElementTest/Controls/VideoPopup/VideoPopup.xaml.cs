9c7f35f59e8997c4d31518fbbaf3ca00f311623c
Jack Schilder <Jack.Schilder@conclusion.nl>
2026-03-31 14:23:43 +0200

    more simplifications

--- src/MediaElementTestApp/MediaElementTest/Controls/VideoPopup/VideoPopup.xaml.cs
+++ src/MediaElementTestApp/MediaElementTest/Controls/VideoPopup/VideoPopup.xaml.cs
@@ -2,22 +2,11 @@ using CommunityToolkit.Maui.Views;
 
 namespace MediaElementTest.Controls;
 
-public partial class VideoPopup : Popup<string?>
+public partial class VideoPopup : Popup
 {
-	private readonly VideoPopupVM _vm;
-
 	public VideoPopup(VideoPopupVM viewModel)
 	{
 		InitializeComponent();		
-		BindingContext = _vm = viewModel;
+		BindingContext = viewModel;
 	}
-
-	//private void OnMediaEnded(object? sender, EventArgs e)
-	//{
-	//	// Minimal code-behind: just delegate to ViewModel command
-	//	if (_vm.CloseCommand.CanExecute(null))
-	//	{
-	//		_vm.CloseCommand.Execute(null);
-	//	}
-	//}
 }
